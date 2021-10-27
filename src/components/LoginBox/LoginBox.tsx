39ff17943bd68c9d90004219926b87b7f06d92b0
davidhaddad7 <davhaddad@ucdavis.edu>
2021-10-27 16:43:52 -0700

    started working on login, not complete yet

---
+++ src/components/LoginBox/LoginBox.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import styles from "./LoginBox.module.scss";
+
+class LoginBox extends React.Component {
+  render() {
+    return (
+      <div >
+          hi
+      </div>
+    );
+  }
+}
+
+export default LoginBox;
---
+++ src/utils/refreshTokenSetup.tsx
@@ -0,0 +1,3 @@
+export const refreshTokenSetup = (res) => {
+    
+};
