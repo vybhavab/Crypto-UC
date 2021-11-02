1fd39460a622014907b9ae31b52152f6d0f9dfd0
swchoi1994 <swchoi1994@gmail.com>
2021-11-02 09:48:16 -0700

    Landing Page Draft 1

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import {
   ChakraProvider,
   Box,
@@ -7,6 +8,11 @@ import {
   VStack,
   Grid,
   theme,
+  Icon, 
+  Button,
+  Flex,
+  useColorModeValue,
+  createIcon
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "../utils/ColorModeSwitcher";
 
@@ -16,14 +22,40 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Heading as="h1">
-            UC-Coin
+          <Heading fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Your Transactions<br />
+            <Text as={'span'} color={'blue.400'}>
+              Secured by ResilientDB
+            </Text>
           </Heading>
-          <Text>
-            The next generation of payments for the UC system. Coming soon...
+          <Text color={'gray.500'}>
+            The Next Generation of Payments for the UC System. 
           </Text>
+            <Button
+            direction={'row'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}
+              colorScheme={'yellow'}
+              bg={'yellow.400'}
+              rounded={'full'}
+              px={8}
+              _hover={{
+                bg: 'yellow.100',
+              }}>
+              Get Started
+            </Button>
+            <Button variant={'https://www.google.com/'} colorScheme={'blue'} size={'md'}>
+              Learn more
+            </Button>
+
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
 )
+
+
