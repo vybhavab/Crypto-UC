d22c0afeabd9477f6acf4f453d9464938dbd428e
davidhaddad7 <davhaddad@ucdavis.edu>
2021-11-06 16:53:38 -0700

    Adding drop down option for dashboard sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
 import React, { ReactNode } from 'react';
 import {
+  Heading,
+  Image,
+  Stack,
+  Button,
+  Divider,
+  Center,
   IconButton,
   Avatar,
   Box,
@@ -13,7 +19,6 @@ import {
   Drawer,
   DrawerContent,
   Text,
-  useDisclosure,
   BoxProps,
   FlexProps,
   Menu,
@@ -34,6 +39,8 @@ import {
 } from 'react-icons/fi';
 import { IconType } from 'react-icons';
 import { ReactText } from 'react';
+import ImageCard from 'components/Sidebar/Sidebar';
+import { useDisclosure } from "@chakra-ui/react";
 
 interface LinkItemProps {
   name: string;
@@ -47,12 +54,14 @@ const LinkItems: Array<LinkItemProps> = [
   { name: 'Settings', icon: FiSettings },
 ];
 
-export default function Sidebar({
+const Sidebar = ({
   children,
 }: {
   children: ReactNode;
-}) {
+}) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = React.useRef() as React.MutableRefObject<HTMLInputElement>;
+
   return (
     <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
       <SidebarContent
@@ -68,7 +77,7 @@ export default function Sidebar({
         onOverlayClick={onClose}
         size="full">
         <DrawerContent>
-          <SidebarContent onClose={onClose} />
+ 
         </DrawerContent>
       </Drawer>
       {/* mobilenav */}
@@ -214,11 +223,95 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
             <MenuList
               bg={useColorModeValue('white', 'gray.900')}
               borderColor={useColorModeValue('gray.200', 'gray.700')}>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
+                    <Center>
+              <Box
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'md'}
+                overflow={'hidden'}>
+                <Image
+                  h={'120px'}
+                  w={'full'}
+                  src={
+                    'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+                  }
+                  objectFit={'cover'}
+                />
+                <Flex justify={'center'} mt={-12}>
+                  <Avatar
+                    size={'xl'}
+                    src={
+                      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+                    }
+                    alt={'Author'}
+                    css={{
+                      border: '2px solid white',
+                    }}
+                  />
+                </Flex>
+
+                <Box p={6}>
+                  <Stack spacing={0} align={'center'} mb={5}>
+                    <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                      John Doe
+                    </Heading>
+                    <Text color={'gray.500'}>Graduate Student</Text>
+                  </Stack>
+
+                  <Stack direction={'row'} justify={'center'} spacing={6}>
+                    <Stack spacing={0} align={'center'}>
+                      <Text fontWeight={600}>40.762</Text>
+                      <Text fontSize={'sm'} color={'gray.500'}>
+                        Balance
+                      </Text>
+                    </Stack>
+                    <Stack spacing={0} align={'center'}>
+                      <Text fontWeight={600}>13.441</Text>
+                      <Text fontSize={'sm'} color={'gray.500'}>
+                        Total Spent
+                      </Text>
+                    </Stack>
+                  </Stack>
+
+                  <Button
+                    w={'full'}
+                    mt={8}
+                    bg={useColorModeValue('#151f21', 'gray.900')}
+                    color={'white'}
+                    rounded={'md'}
+                    _hover={{
+                      transform: 'translateY(-2px)',
+                      boxShadow: 'lg',
+                    }}>
+                    Manage your profile
+                  </Button>
+                  <Divider paddingTop="3"/>
+
+                  <VStack
+                    divider={<Divider />}
+                    align="left"
+                  >
+                    <Box h="30px" paddingTop="1">
+                      Settings
+                    </Box>
+                    <Box h="30px" >
+                      Appearance
+                    </Box>
+                    <Box h="30px" >
+                      Tax {"&"} Reports
+                    </Box>
+                    <Box h="30px" >
+                      Help
+                    </Box>
+                    <Box h="30px" color="red" >
+                      Logout
+                    </Box>
+                  </VStack>
+
+                </Box>
+              </Box>
+            </Center>
             </MenuList>
           </Menu>
         </Flex>
@@ -226,3 +319,5 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
     </Flex>
   );
 };
+
+export default Sidebar;
