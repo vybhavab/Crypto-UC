d087a5b00823224e6871c878359567d5fd6a345f
davidhaddad7 <davhaddad@ucdavis.edu>
2021-11-29 01:56:39 -0800

    adding coming soon

--- src/pages/Dashboard/InviteOthers.tsx
+++ src/pages/Dashboard/InviteOthers.tsx
@@ -1,10 +1,15 @@
+import { Grid, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const InviteOthers = () =>{
     return (
-        <div>
-            InviteOthers
-        </div>
+        <Grid minH="90vh" p={3}>
+            <VStack spacing={10}>
+                 <Heading pt={10} as="h1" size='lg'>
+                     Coming Soon...
+                 </Heading>
+            </VStack>
+        </Grid>
     )
 }
 export default InviteOthers;
--- src/pages/Dashboard/Market.tsx
+++ src/pages/Dashboard/Market.tsx
@@ -1,10 +1,15 @@
+import { Box, Grid, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const Market = () => {
     return (
-        <div>
-            Market
-        </div>
+        <Grid minH="90vh" p={3}>
+            <VStack spacing={10}>
+                 <Heading pt={10} as="h1" size='lg'>
+                    Coming Soon...
+                 </Heading>
+            </VStack>
+        </Grid>
     )
 }
 export default Market;
